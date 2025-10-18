0244ca77eb74ea4874de67592b3f91db12ad02bf
chrizxzayax <chmolina629@hotmail.com>
2025-10-18 00:01:21 -0700

    like usual... the problem seemed to be just an error on the compiler not the code itself

--- midterm2.cpp
+++ midterm2.cpp
@@ -11,25 +11,26 @@
 using namespace std;
 
 struct Node {
-  string name; // customer's name
-  bool isVIP;
-  Node *prev;
-  Node *next;
-  Node(string n, bool v, Node *p = nullptr, Node *nx = nullptr) // constructor
-      : name(n), isVIP(v), prev(p), next(nx) {}
+    string name; // customer's name
+    bool isVIP;
+    Node* prev;
+    Node* next;
+    Node(string n, bool v=false, Node* p=nullptr, Node* nx=nullptr) // constructor
+        : name(n), isVIP(v), prev(p), next(nx) {}
 };
+
 class DoublyLinkedList {
 private:
-  Node *head; // pointer to the start of the list
-  Node *tail; // pointer to the end of the list
+  Node* head; // pointer to the start of the list
+  Node* tail; // pointer to the end of the list
 
 public:
   DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
   // ill add here all the methods needed for the doubly linked list
 
-  void push_back(const string &name, bool isVIP = false) { // method to add a customer at the end of the list
-    Node *newNode = new Node(name, isVIP);
+  void push_back(const string& name, bool isVIP=false) { // method to add a customer at the end of the list
+    Node* newNode = new Node(name, isVIP);
     if (!tail) {
       head = tail = newNode;
     } else {
@@ -40,8 +41,8 @@ public:
   }
 
   // add the customer at front
-  void push_front(const string &name, bool isVIP = false) {
-    Node *newNode = new Node(name, isVIP);
+  void push_front(const string& name, bool isVIP=false) {
+    Node* newNode = new Node(name, isVIP);
     if (!head)
       head = tail = newNode;
     else {
@@ -51,9 +52,9 @@ public:
     }
   }
 
-  string pop_front(bool &wasVIP) {
+  string pop_front(bool& wasVIP) {
     if (!head) return "";
-    Node *temp = head;
+    Node* temp = head;
     string nm = temp->name;
     wasVIP = temp->isVIP;
     if (head->next) {
@@ -66,7 +67,7 @@ public:
     return nm;// return the name of the served customer
   }
 
-  string pop_back(bool &wasVIP) {
+  string pop_back(bool& wasVIP) {
     if (!tail) return "";
     Node* temp = tail;
     string nm = temp->name;
@@ -86,7 +87,7 @@ public:
         if (sz <= 2) return "";
         int pos = rand() % (sz-2) + 2; // skip head and tail
         Node* temp = head;
-        for (int i = 1; i < pos; ++i)
+        for (int i = 1; i < pos; ++i)// traverse to the node at position pos
             temp = temp->next;
         string nm = temp->name;
         wasVIP = temp->isVIP;
@@ -104,11 +105,11 @@ void print() const {
             cout << current->name << (current->isVIP ? " (VIP)" : "") << endl;
             current = current->next;
         }
-}
+    }
 
   int size() const {
     int count = 0;
-    Node *current = head;
+    Node* current = head;
     while (current) {
       count++;
       current = current->next;
@@ -139,10 +140,10 @@ string pick_random_name(vector<string>& names) {
 int main() {
   srand(time(0)); // seed for randomness
 
-  vector<string> names;
+    vector<string> names;
     ifstream fin("names.txt");
-    string line;
-    while (getline(fin, line)) if (!line.empty()) names.push_back(line);
+    string fileLine;
+    while (getline(fin, fileLine)) if (!fileLine.empty()) names.push_back(fileLine);
 
     DoublyLinkedList line;
     int TIME_STEPS = 20;
@@ -157,13 +158,29 @@ int main() {
     cout << "Resulting line:\n";
     line.print();
 
-  for (int t = 2; t <= TIME_STEPS; ++t) {
-    cout << "Time step #" << t << ":\n";
-    // 40%: customer is served at front
-    int prob = rand() % 100 + 1;
-    if (prob <= 40 && !line.empty()) {
-      bool wasVIP;
-    }
+   for (int t = 2; t <= TIME_STEPS; ++t) {
+        cout << "Time step #" << t << ":\n";
+        // 40%: customer is served at front
+        int prob = rand() % 100 + 1;
+        if (prob <= 40 && !line.empty()) {
+            bool wasVIP;
+            string served = line.pop_front(wasVIP);
+            cout << served << " is served" << endl;
+        }
+        // 60%: new customer joins at rear
+        prob = rand() % 100 + 1;
+        if (prob <= 60 && !names.empty()) {
+            string cname = pick_random_name(names);
+            line.push_back(cname);
+            cout << cname << " joins the line" << endl;
+        }
+        // 20%: end customer gets frustrated and leaves
+        prob = rand() % 100 + 1;
+        if (prob <= 20 && !line.empty()) {
+            bool wasVIP;
+            string left = line.pop_back(wasVIP);
+            cout << left << " (at the rear) left the line" << endl;
+        }
   }
 
   return 0;

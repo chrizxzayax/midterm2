20349205c09fa96d457c1141a44dbcab869d41e0
chrizxzayax <chmolina629@hotmail.com>
2025-10-17 23:41:14 -0700

    right now im trying to show the values correctly

--- midterm2.cpp
+++ midterm2.cpp
@@ -1,86 +1,90 @@
 // comsc 210 | midterm2 | Christian Molina
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
 struct Node {
-    string name;// customer's name
-    bool isVIP;
-    Node* prev;
-    Node* next;
-    Node(string n, bool v, Node* p = nullptr, Node* ne = nullptr)// constructor
-    : name(n), isVIP(v), prev(p), next(ne) {}
+  string name; // customer's name
+  bool isVIP;
+  Node *prev;
+  Node *next;
+  Node(string n, bool v, Node *p = nullptr, Node *ne = nullptr) // constructor
+      : name(n), isVIP(v), prev(p), next(ne) {}
 };
 class DoublyLinkedList {
-    private:
-        Node* head;// pointer to the start of the list
-        Node* tail;// pointer to the end of the list
+private:
+  Node *head; // pointer to the start of the list
+  Node *tail; // pointer to the end of the list
 
-    public:
-        DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+public:
+  DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
-        // ill add here all the methods needed for the doubly linked list
+  // ill add here all the methods needed for the doubly linked list
 
-        void push_back(const string& name, bool isVIP) {// method to add a customer at the end of the list
-            Node* newNode = new Node(name, isVIP);
-            if (!tail) {
-                head = tail = newNode;
-            } else {
-                tail->next = newNode;
-                newNode->prev = tail;
-                tail = newNode;
-            }
-        }
+  void push_back(const string &name,
+            bool isVIP) { // method to add a customer at the end of the list
+    Node *newNode = new Node(name, isVIP);
+    if (!tail) {
+      head = tail = newNode;
+    } else {
+      tail->next = newNode;
+      newNode->prev = tail;
+      tail = newNode;
+    }
+  }
 
-        // add the customer at front
-        void push_front(const string& name, bool isVIP=false) {
-            Node* newNode = new Node(name, isVIP);
-            if (!head)
-                head = tail = newNode;
-            else {
-                newNode->next = head;
-                head->prev = newNode;
-                head = newNode;
-            }
-        }
+  // add the customer at front
+  void push_front(const string &name, bool isVIP = false) {
+    Node *newNode = new Node(name, isVIP);
+    if (!head)
+      head = tail = newNode;
+    else {
+      newNode->next = head;
+      head->prev = newNode;
+      head = newNode;
+    }
+  }
 
-        string pop_front(bool& wasVIP) {
-        if (!head) return "";
-        Node* temp = head;
-        string nm = temp->name;
-        wasVIP = temp->isVIP;
-        if (head->next) {
-            head = head->next;
-            head->prev = nullptr;
-        } else {
-            head = tail = nullptr;
-        }
-        delete temp;
-        return nm;
-        }
+  string pop_front(bool &wasVIP) {
+    if (!head)
+      return "";
+    Node *temp = head;
+    string nm = temp->name;
+    wasVIP = temp->isVIP;
+    if (head->next) {
+      head = head->next;
+      head->prev = nullptr;
+    } else {
+      head = tail = nullptr;
+    }
+    delete temp;
+    return nm;
+  }
 
-        string pop_back(bool& wasVIP) {
-            if (!tail) return "";
-            Node* temp = tail;
-            string nm = temp->name;
-            wasVIP = temp->isVIP;
-            if (tail->prev) {
-                tail = tail->prev;
-                tail->next = nullptr;
-            } else {
-                head = tail = nullptr;
-            }
-            delete temp;
-            return nm;
-        }
+  string pop_back(bool &wasVIP) {
+    if (!tail)
+      return "";
+    Node *temp = tail;
+    string nm = temp->name;
+    wasVIP = temp->isVIP;
+    if (tail->prev) {
+      tail = tail->prev;
+      tail->next = nullptr;
+    } else {
+      head = tail = nullptr;
+    }
+    delete temp;
+    return nm;
+  }
 
-        string remove_random(bool& wasVIP) {
+ string remove_random(bool& wasVIP) {
         int sz = size();
         if (sz <= 2) return "";
         int pos = rand() % (sz-2) + 2; // skip head and tail
@@ -95,19 +99,36 @@ class DoublyLinkedList {
         return nm;
     }
 
-        int size() const {
-                int count = 0;
-                Node* current = head;
-                while (current) {
-                    count++;
-                    current = current->next;
-                }
-                return count;
-            }
+void print() const {
 
-        bool empty () const {
-            return head == nullptr;
+     Node* current = head;
+        if (!current) { cout << "Line is empty.\n"; return; }
+        while (current) {
+            cout << current->name << (current->isVIP ? " (VIP)" : "") << endl;
+            current = current->next;
         }
+}
+
+  int size() const {
+    int count = 0;
+    Node *current = head;
+    while (current) {
+      count++;
+      current = current->next;
+    }
+    return count;
+  }
+
+    bool empty() const {
+        return head == nullptr;
+    }
+    string front() const {
+        return head ? head->name : "";
+    }
+    string back() const {
+        return tail ? tail->name : "";
+    }
+
 };
 
 string pick_random_name(vector<string>& names) {
@@ -118,37 +139,36 @@ string pick_random_name(vector<string>& names) {
     return name;
 }
 
-int main (){
-    srand(time(0)); // seed for randomness
+int main() {
+  srand(time(0)); // seed for randomness
 
-    vector<string> names;
-    ifstream fin("names.txt");// open the file
-    string line;
+  vector<string> names;
+  ifstream fin("names.txt"); // open the file
+  string line;
 
-    while (getline(fin, line)) 
+  while (getline(fin, line))
     if (!line.empty()) {
-        names.push_back(line);// store each name in the vector
+      names.push_back(line); // store each name in the vector
     }
 
-    int TIME_STEPS = 20; // number of time steps to simulate
-    
-    cout << "store opens: \n";
-    for (int i = 0; i < 5; ++i) {
-        string cname = pick_random_name(names);
-        cout << cname << " joins the line\n";
+  int TIME_STEPS = 20; // number of time steps to simulate
+
+  cout << "store opens: \n";
+  for (int i = 0; i < 5; ++i) {
+    string cname = pick_random_name(names);
+    cout << cname << " joins the line\n";
+  }
+  cout << "Resulting line:\n";
+  line.print();
+
+  for (int t = 2; t <= TIME_STEPS; ++t) {
+    cout << "Time step #" << t << ":\n";
+    // 40%: customer is served at front
+    int prob = rand() % 100 + 1;
+    if (prob <= 40 && !line.empty()) {
+      bool wasVIP;
     }
-    cout << "Resulting line:\n";
+  }
 
-
-     for (int t = 2; t <= TIME_STEPS; ++t) {
-        cout << "Time step #" << t << ":\n";
-        // 40%: customer is served at front
-        int prob = rand() % 100 + 1;
-        if (prob <= 40 && !line.empty()) {
-            bool wasVIP;
-
-        }
-     }
-
-    return 0;
+  return 0;
 }

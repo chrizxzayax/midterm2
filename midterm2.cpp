3cbbe865039f050bc82a99b0611e1f74653bf5f0
chrizxzayax <chmolina629@hotmail.com>
2025-10-17 23:20:31 -0700

    interestingly popfront is not working like in the last project

--- midterm2.cpp
+++ midterm2.cpp
@@ -1,12 +1,11 @@
 // comsc 210 | midterm2 | Christian Molina
 
-#include <fstream>
 #include <iostream>
-#include <string>
+#include <fstream>
 #include <vector>
+#include <string>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 
 using namespace std;
 
@@ -19,42 +18,67 @@ struct Node {
     : name(n), VIP(v), prev(p), next(ne) {}
 };
 class DoublyLinkedList {
-private:
-    Node* head;// pointer to the start of the list
-    Node* tail;// pointer to the end of the list
+    private:
+        Node* head;// pointer to the start of the list
+        Node* tail;// pointer to the end of the list
 
-public:
-    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+    public:
+        DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
-    // ill add here all the methods needed for the doubly linked list
+        // ill add here all the methods needed for the doubly linked list
 
-    void push_back(const string& name, bool isVIP) {// method to add a customer at the end of the list
-        Node* newNode = new Node(name, isVIP);
-        if (!tail) {
-            head = tail = newNode;
-        } else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
+        void push_back(const string& name, bool VIP) {// method to add a customer at the end of the list
+            Node* newNode = new Node(name, VIP);
+            if (!tail) {
+                head = tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
         }
-    }
 
-    // add the customer at front
-    void push_front(const string& name, bool isVIP=false) {
-        Node* newNode = new Node(name, isVIP);
-        if (!head)
-            head = tail = newNode;
-        else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
+        // add the customer at front
+        void push_front(const string& name, bool VIP=false) {
+            Node* newNode = new Node(name, VIP);
+            if (!head)
+                head = tail = newNode;
+            else {
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+            }
         }
-    }
+
+        void pop_front(bool& wasVIP) {// method to remove the customer at the front of the list
+            if (!head) return "";
+            Node* temp = head;
+            string nm = temp->name;
+                cout << "Line is empty." << endl;
+                return;
+            }
+            Node* temp = head;
+            wasVIP = temp->VIP;
+            if (head->next) {
+                head = head->next;
+                head->prev = nullptr;
+            } else
+                head = tail = nullptr;
+            delete temp;
+            return nm;
+        }
+
+        bool empty () const {
+            return head == nullptr;
+        }
 };
 
-string pick_random_name(const vector<string>& names) {
-    int index = rand() % names.size();
-    return names[index];
+string pick_random_name(vector<string>& names) {
+    if (names.empty()) return "Anonymous";
+    int idx = rand() % names.size();
+    string name = names[idx];
+    names.erase(names.begin() + idx);
+    return name;
 }
 
 int main (){
@@ -69,12 +93,16 @@ int main (){
         names.push_back(line);// store each name in the vector
     }
 
-    DoublyLinkedList line;
     int TIME_STEPS = 20; // number of time steps to simulate
-    for (const string& name : names) {
-        bool isVIP = (rand() % 2 == 0); // Randomly assign VIP status
+    
+    cout << "store opens: \n";
+    for (int i = 0; i < 5; ++i) {
+        string cname = pick_random_name(names);
+        line.push_back(cname);
+        cout << cname << " joins the line\n";
     }
-
+    cout << "Resulting line:\n";
+    line.print();
 
     return 0;
 }
